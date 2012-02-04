ac1f6dd9f95f1d14d8304884212d5eb2c760a41f
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-02-03 18:38:51 -0800

    RotateControl: Initial commit.

---
+++ RotateControl/src/com/swijaya/android/rotatecontrol/RotateControlActivity.java
@@ -0,0 +1,13 @@
+package com.swijaya.android.rotatecontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RotateControlActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
